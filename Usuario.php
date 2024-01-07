4313401500f79087fbd71bf68c7e105a1e6382eb
Sebastian Moises Jurupe Vilchez <122936900+SebastianJurupe@users.noreply.github.com>
2024-01-07 12:57:28 -0500

    Termino de CRUD

--- Usuario.php
+++ Usuario.php
@@ -25,5 +25,47 @@ class Usuario
 
   public function getUsuarios()
   {
+    $query = "SELECT * FROM usuario";
+    $stmt = $this->connection->prepare($query);
+
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+  }
+
+  public function updateUser($data)
+  {
+    $query = "UPDATE usuario SET nombre=:nombre, telefono=:telefono, email=:email WHERE id=:id";
+    $stmt = $this->connection->prepare($query);
+
+    $stmt->bindValue(':nombre', $data['nombre']);
+    $stmt->bindValue(':telefono', $data['telefono']);
+    $stmt->bindValue(':email', $data['email']);
+    //vincular el valor de :id
+    $stmt->bindValue(':id', $data['id']); 
+    $stmt->execute();
+  }
+
+  public function getUnicUser($id){
+    $query = "SELECT * FROM usuario WHERE id=:id";
+    $stmt = $this->connection->prepare($query);
+
+    $stmt->execute([
+      ":id" => $id,
+    ]);
+
+    $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $resultado;
+  }
+
+  public function deldeteUser($id){
+    $query = "DELETE FROM usuario WHERE id=:id";
+    $stmt = $this->connection->prepare($query);
+
+    $stmt->execute([
+      ":id" => $id,
+    ]);
+
+    return $stmt;
   }
 }
--- sistema.php
+++ sistema.php
@@ -2,8 +2,30 @@
 require_once("autoload.php");
 $objUsuario = new Usuario();
 
-$objUsuario->insertUsuario([
-  "nombre" => "Sebastian",
-  "telefono" => 987654123,
-  "email" => "weoqweqwe@gmail.com",
-]);
+// $objUsuario->insertUsuario([
+//   "nombre" => "Sebastian",
+//   "telefono" => 987654123,
+//   "email" => "weoqweqwe@gmail.com",
+// ]);
+
+//Extrae todos los registros
+$users = $objUsuario->getUsuarios();
+print_r("<pre>");
+print_r($users);
+print_r("<pre>");
+
+//Update User
+// $objUsuario->updateUser([
+//   "nombre"=>"Roxana",
+//   "telefono"=>21312321,
+//   "email"=>"qweqw@gmail.com",
+//   "id"=>2,
+// ]);
+
+// //Extrae unico registro
+// $unic = $objUsuario->getUnicUser(3);
+// print_r("<pre>");
+// print_r($unic);
+// print_r("<pre>");
+
+$delete = $objUsuario->deldeteUser(2);
